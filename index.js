a6bae1b593a7c20b0e4813c7a420b83c4e371ce6
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-04-04 13:45:23 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      renderBooks(data);
+    })
+    .catch((error) => {
+      console.error("There was a problem fetching the books:", error.message);
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
